7e4de786ac2989a54441c1c3c27feea4970f78ce
kennathanael <knathanael3@gmail.com>
2022-12-21 16:36:36 +0700

    navbar

---
+++ resources/views/components/input-field.blade.php
@@ -0,0 +1,4 @@
+<div class="input-group mb-3">
+    <span class="input-group-text" id="inputGroup-sizing-default">@yield</span>
+    <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+  </div>
